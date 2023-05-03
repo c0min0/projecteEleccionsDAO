0d5964c02244142d63b49d2b8ffb13d28c5ef880
BenitoMF <b.martinez2@sapalomera.cat>
2023-05-03 11:31:33 +0200

    Metodes introduits DAO

--- src/exercicis/taules/DAO/CandidatDAO.java
+++ src/exercicis/taules/DAO/CandidatDAO.java
@@ -1,4 +1,42 @@
 package exercicis.taules.DAO;
 
-public class CandidatDAO {
+import exercicis.taules.Candidat;
+
+import java.util.List;
+
+public class CandidatDAO implements DAODB<Candidat> {
+    @Override
+    public boolean create(Candidat candidat) {
+        return false;
+    }
+
+    @Override
+    public boolean read(Candidat candidat) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Candidat candidat) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Candidat candidat) {
+        return false;
+    }
+
+    @Override
+    public boolean exists(Candidat candidat) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+
+    @Override
+    public List<Candidat> all() {
+        return null;
+    }
 }
--- src/exercicis/taules/DAO/CandidaturaDAO.java
+++ src/exercicis/taules/DAO/CandidaturaDAO.java
@@ -1,4 +1,42 @@
 package exercicis.taules.DAO;
 
-public class CandidaturaDAO {
+import exercicis.taules.Candidatura;
+
+import java.util.List;
+
+public class CandidaturaDAO implements DAODB<Candidatura> {
+    @Override
+    public boolean create(Candidatura candidatura) {
+        return false;
+    }
+
+    @Override
+    public boolean read(Candidatura candidatura) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Candidatura candidatura) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Candidatura candidatura) {
+        return false;
+    }
+
+    @Override
+    public boolean exists(Candidatura candidatura) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+
+    @Override
+    public List<Candidatura> all() {
+        return null;
+    }
 }
