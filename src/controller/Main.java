91c857df2a3efb61ca816b75fb6ff323884e4ee9
Víctor Comino <v.comino@sapalomera.cat>
2023-05-15 12:05:17 +0200

    .

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,7 +3,6 @@ package controller;
 import static view.Print.println;
 
 //TODO: Implementar comAutonomaDAO
-//TODO: Fer package controller.comAutonoma
 
 public class Main {
     public static void main(String[] args) {
