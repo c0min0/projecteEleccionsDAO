0d371efd912dcdc73f930361539232a7833af819
Benito44 <b.martinez2@sapalomera.cat>
2023-05-16 11:21:43 +0200

    Revisió de ControllerComAutonoma

--- src/controller/DAO/mySQL/ComAutonomaDAO.java
+++ src/controller/DAO/mySQL/ComAutonomaDAO.java
@@ -1,7 +1,6 @@
 package controller.DAO.mySQL;
 
 import controller.DAO.DAODB;
-import model.Candidatura;
 import model.ComAutonoma;
 
 import java.util.LinkedList;
@@ -16,7 +15,7 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // INSERT SQL
         String query = "INSERT INTO comunitats_autonomes (nom,codi_ine) VALUES (?,?)";
 
-        // Escrivim a la BD amb els valors de la candidatura passada per paràmetres
+        // Escrivim a la BD amb els valors de la comunitat_autonoma passada per paràmetres
         int r = DBMySQLManager.write(query,
                 CA.getNom(),
                 CA.getCodiIne());
@@ -27,13 +26,13 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
 
     @Override
     public boolean read(ComAutonoma CA) {
-        // Obtenim una candidatura de la BD amb el mateix id que la candidatura passada per paràmetres
+        // Obtenim una comunitat_autonoma de la BD amb el mateix id que la comunitat_autonoma passada per paràmetres
         ComAutonoma CAr = readById(CA.getId());
 
         // Si no existeix, retornem false
         if (CAr == null) return false;
 
-        // Si existeix, actualitzem la candidatura passada per paràmetres amb els valors de la candidatura de la BD
+        // Si existeix, actualitzem la comunitat_autonoma passada per paràmetres amb els valors de la comunitat_autonoma de la BD
         CA.set(CAr.getNom(),CAr.getCodiIne());
 
         // i retornem true
@@ -46,13 +45,13 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // SELECT SQL
         String query = "SELECT nom, codi_ine FROM comunitats_autonomes WHERE comunitat_aut_id=?";
 
-        // Llegim de la BD amb l'id de la candidatura passada per paràmetres
+        // Llegim de la BD amb l'id de la comunitat_autonoma passada per paràmetres
         List<Object[]> r = DBMySQLManager.read(query, id);
 
         // Si no existeix, retornem null
         if (r.size() != 1L) return null;
         //
-        //        // Si existeix, retornem la candidatura
+        //        // Si existeix, retornem la comunitat_autonoma
         Object[] row = r.iterator().next();
 
         String nom = (String)row[0];
@@ -67,12 +66,12 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // UPDATE SQL
         String query = "UPDATE comunitats_autonomes SET nom=?,codi_ine=? WHERE comunitat_aut_id=?";
 
-        // Actualitzem la BD amb els valors de la candidatura passada per paràmetres
-        String nom_curt = CA.getNom();
+        // Actualitzem la BD amb els valors de la comunitat_autonoma passada per paràmetres
+        String nom = CA.getNom();
         String codi_ine = CA.getCodiIne();
         int comunitat_aut_id = CA.getId();
 
-        int r = DBMySQLManager.write(query, codi_ine, comunitat_aut_id);
+        int r = DBMySQLManager.write(query, nom,codi_ine, comunitat_aut_id);
 
         // Si s'ha modificat alguna fila, retornem true
         return r > 0;
@@ -80,7 +79,7 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
 
     @Override
     public boolean update(ComAutonoma CA, String... camps) {
-        // Creem un array de paràmetres de la mida de la quantitat de camps + 1 (per la candidatura_id)
+        // Creem un array de paràmetres de la mida de la quantitat de camps + 1 (per la comunitat_aut_id)
         Object[] params = new Object[camps.length + 1];
 
         // Construïm la query
@@ -120,7 +119,7 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // DELETE SQL
         String query = "DELETE FROM comunitats_autonomes WHERE comunitat_aut_id=?";
 
-        // Eliminem de la BD la candidatura passada per paràmetres
+        // Eliminem de la BD la comunitat_autonoma passada per paràmetres
         int r = DBMySQLManager.write(query, CA.getId());
 
         // Si s'ha eliminat alguna fila, retornem true
@@ -128,10 +127,10 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
 
     @Override
     public List<ComAutonoma> search(String camp, Object valor) {
-        // Creem una llista buida de Candidatures
+        // Creem una llista buida de ComunitatsAutonomes
         List<ComAutonoma> l = new LinkedList<>();
 
-        // Query per obtenir totes les candidatures amb el valor del camp passat per paràmetres
+        // Query per obtenir totes les ComunitatsAutonomes amb el valor del camp passat per paràmetres
         String query = "SELECT * FROM comunitats_autonomes WHERE " + camp + "=?";
 
         // Executem la query
@@ -140,12 +139,12 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // Per cada registre
         for (Object[] row : r) {
 
-            // Obtenim les dades de la candidatura
+            // Obtenim les dades de la comunitat_autonoma
             int comunitat_aut_id = (int) row[0];
             String nom = (String) row[1];
             String codi_ine = (String) row[2];
 
-            // Afegim la candidatura a la llista
+            // Afegim la comunitat_autonoma a la llista
             l.add(new ComAutonoma(comunitat_aut_id, nom, codi_ine));
 
         }
@@ -154,9 +153,9 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         return l;
     }
     /**
-     * Comprova si una candidatura existeix a la BD.
-     * @param CA Candidatura a comprovar.
-     * @return true si la candidatura existeix a la BD, false si no.
+     * Comprova si una comunitat_autonoma existeix a la BD.
+     * @param CA ComunitatAutonoma a comprovar.
+     * @return true si la comunitat_autonoma existeix a la BD, false si no.
      */
 
     @Override
@@ -165,8 +164,8 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
     }
 
     /**
-     * Fa el recompte de registres a la taula candidatures.
-     * @return nombre de registres de la taula candidatures.
+     * Fa el recompte de registres a la taula comunitats_autonomes.
+     * @return nombre de registres de la taula comunitats_autonomes.
      */
 
     @Override
@@ -194,7 +193,7 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // Creem una llista buida de persones
         List<ComAutonoma> l = new LinkedList<>();
 
-        // Query per obtenir totes les candidatures
+        // Query per obtenir totes les comunitats_autonomes de la BD
         String query = "SELECT comunitat_aut_id, nom, codi_ine FROM comunitats_autonomes";
 
         // Executem la query
@@ -203,12 +202,12 @@ public class ComAutonomaDAO implements DAODB<ComAutonoma> {
         // Per cada registre
         for (Object[] row : r) {
 
-            // Obtenim les dades de la candidatura
+            // Obtenim les dades de la comunitat_autonoma
             int comunitat_aut_id = (int) row[0];
             String nom = (String) row[1];
             String codi_ine = (String) row[2];
 
-            // Afegim la candidatura a la llista
+            // Afegim la comunitat_autonoma a la llista
             l.add(new ComAutonoma(comunitat_aut_id, nom,codi_ine));
         }
 
