c7744bff74a189ae226be91f9a6d08dec285549b
Víctor Comino <v.comino@sapalomera.cat>
2023-05-18 10:43:56 +0200

    .

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,14 +2,12 @@ package controller;
 
 import static view.Print.println;
 
-//TODO: Implementar comAutonomaDAO
-
 public class Main {
     public static void main(String[] args) {
-        //try {
+        try {
             Controller.menuInicial();
-       // } catch (Exception e) {
-         //   println("Ha hagut una problema, disculpa les molèsties. :(");
-        //}
+        } catch (Exception e) {
+            println("Ha hagut una problema, disculpa les molèsties. :(");
+        }
     }
 }
